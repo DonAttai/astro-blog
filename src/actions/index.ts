5b82787e90638d07b0049c901cd0e5e5479ac944
John Attai Yinusa <donattai1032@gmail.com>
2025-01-08 07:14:04 +0100

    add auth layout, login and register pages

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -7,7 +7,7 @@ export const server = {
   commentAction: defineAction({
     input: z.object({
       content: z.string().min(3, "Comment must be at least 3 characters"),
-      article_id: z.string(),
+      articleId: z.string(),
       author: z.string(),
     }),
     handler: async (input) => {
